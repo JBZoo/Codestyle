4222b2de1273d486a4cbfc45e5b1f2d935cc419a
Denis Smetannikov <smet.denis@gmail.com>
2020-07-15 01:26:03 +0300

    New versions

---
+++ tests/CodestyleComposerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * JBZoo Toolbox - Codestyle
+ *
+ * This file is part of the JBZoo Toolbox project.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package    Codestyle
+ * @license    MIT
+ * @copyright  Copyright (C) JBZoo.com, All rights reserved.
+ * @link       https://github.com/JBZoo/Codestyle
+ * @author     Denis Smetannikov <denis@jbzoo.com>
+ */
+
+namespace JBZoo\PHPUnit;
+
+/**
+ * Class CodestyleComposerTest
+ * @package JBZoo\PHPUnit
+ */
+class CodestyleComposerTest extends AbstractComposerTest
+{
+}
