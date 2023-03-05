d12e2f6fde02a6ccbbd8520b9aa4ce5084701697
Denis Smet <denis.smetannikov@yandex.ru>
2023-03-05 16:32:49 +0400

    Fixed test

--- tests/PhpUnitDefinesTest.php
+++ tests/PhpUnitDefinesTest.php
@@ -16,6 +16,8 @@ declare(strict_types=1);
 
 namespace JBZoo\PHPUnit;
 
+use JBZoo\CodeStyle\PhpCsFixer\PhpCsFixerCodingStandard;
+
 /**
  * @internal
  * @coversNothing
@@ -120,5 +122,28 @@ final class PhpUnitDefinesTest extends PHPUnit
                 'tests/',
             ],
         ], $configs);
+    }
+
+    public function testPhpCsFixerConfig(): void
+    {
+        $config = (new PhpCsFixerCodingStandard(__DIR__ . '/..'))
+            ->getFixerConfig();
+
+        isTrue($config->getUsingCache());
+        isContain('/build/php-cs-fixer-cache.json', $config->getCacheFile());
+        isSame('    ', $config->getIndent());
+        isSame("\n", $config->getLineEnding());
+        isSame('JBZoo Style', $config->getName());
+    }
+
+    public function testPhpCsFixerConfigAsFile(): void
+    {
+        $config = include \realpath(__DIR__ . '/../src/PhpCsFixer/php-cs-fixer.php');
+
+        isTrue($config->getUsingCache());
+        isContain('/build/php-cs-fixer-cache.json', $config->getCacheFile());
+        isSame('    ', $config->getIndent());
+        isSame("\n", $config->getLineEnding());
+        isSame('JBZoo Style', $config->getName());
     }
 }
