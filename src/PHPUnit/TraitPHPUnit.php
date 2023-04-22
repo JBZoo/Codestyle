189279d4e2a3e859f96a9b40e401c6ed8f44bf58
Denis Smet <denis.smetannikov@yandex.ru>
2023-04-23 02:44:47 +0400

    Rollback to PHPUnit ^9.6

--- src/PHPUnit/TraitPHPUnit.php
+++ src/PHPUnit/TraitPHPUnit.php
@@ -27,32 +27,32 @@ use function JBZoo\PHPUnit\isSame;
  */
 trait TraitPHPUnit
 {
-//    public function testBootstrap(): void
-//    {
-//        isSame('tests/autoload.php', self::getPhpUnitAsArray()->find('_children.0._attrs.bootstrap'));
-//    }
-//
-//    public function testColor(): void
-//    {
-//        dump(self::getPhpUnitAsArray()->find('_children.0._attrs'));
-//
-//        isSame('true', self::getPhpUnitAsArray()->find('_children.0._attrs.colors1'));
-//    }
-//
-//    public function testVerbose(): void
-//    {
-//        isSame('true', self::getPhpUnitAsArray()->find('_children.0._attrs.verbose'));
-//    }
-//
-//    public function testXmlnsXsi(): void
-//    {
-//        isSame('true', self::getPhpUnitAsArray()->find('_children.0._attrs.xmlns:xsi'));
-//    }
-//
-//    private static function getPhpUnitAsArray(): Data
-//    {
-//        $xml = \file_get_contents(PROJECT_ROOT . '/phpunit.xml.dist');
-//
-//        return data(Xml::dom2Array(Xml::createFromString($xml)));
-//    }
+    //    public function testBootstrap(): void
+    //    {
+    //        isSame('tests/autoload.php', self::getPhpUnitAsArray()->find('_children.0._attrs.bootstrap'));
+    //    }
+    //
+    //    public function testColor(): void
+    //    {
+    //        dump(self::getPhpUnitAsArray()->find('_children.0._attrs'));
+    //
+    //        isSame('true', self::getPhpUnitAsArray()->find('_children.0._attrs.colors1'));
+    //    }
+    //
+    //    public function testVerbose(): void
+    //    {
+    //        isSame('true', self::getPhpUnitAsArray()->find('_children.0._attrs.verbose'));
+    //    }
+    //
+    //    public function testXmlnsXsi(): void
+    //    {
+    //        isSame('true', self::getPhpUnitAsArray()->find('_children.0._attrs.xmlns:xsi'));
+    //    }
+    //
+    //    private static function getPhpUnitAsArray(): Data
+    //    {
+    //        $xml = \file_get_contents(PROJECT_ROOT . '/phpunit.xml.dist');
+    //
+    //        return data(Xml::dom2Array(Xml::createFromString($xml)));
+    //    }
 }
